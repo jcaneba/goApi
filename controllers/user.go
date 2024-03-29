0a5f69caa3cbab3f266a4580ab449abfd9143c5a
jcaneba <jesuscanebatic@gmail.com>
2024-03-29 13:40:23 +0100

    Nuevos métodos PATCH y DELETE para la actualización y borrado de registros

--- controllers/user.go
+++ controllers/user.go
@@ -5,14 +5,30 @@ package controllers
 import (
 	"goApi/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"gorm.io/gorm"
 )
 
-func CreateUsers(c *gin.Context, db *gorm.DB) {
+func CreateUsersTable(c *gin.Context, db *gorm.DB) {
 	db.AutoMigrate(models.User{})
+}
+
+func CreateUsers(c *gin.Context, db *gorm.DB) {
+	var users []models.User
+	if err := c.BindJSON(&users); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	for _, user := range users {
+		if err := db.Create(&user).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al crear el usuario"})
+			return
+		}
+	}
+	c.JSON(http.StatusCreated, users)
 }
 
 func GetUsers(c *gin.Context, db *gorm.DB) {
@@ -30,7 +46,7 @@ func GetUserId(c *gin.Context, db *gorm.DB) {
 	}
 
 	if err := db.First(&user, userIDRequest.UserID).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no encontrado"})
 		return
 	}
 	c.JSON(http.StatusOK, user)
@@ -57,8 +73,52 @@ func PostUserId(c *gin.Context, db *gorm.DB) {
 	}*/
 
 	if err := db.First(&user, userIDRequest.UserID).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no encontrado"})
 		return
 	}
 	c.JSON(http.StatusOK, user)
+}
+
+func UpdateUser(c *gin.Context, db *gorm.DB) {
+	var updatedUser models.User
+	if err := c.BindJSON(&updatedUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Verificar si el usuario existe
+	var existingUser models.User
+	if err := db.First(&existingUser, updatedUser.ID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no encontrado"})
+		return
+	}
+
+	// Actualizar el usuario en la base de datos
+	if err := db.Model(&existingUser).Updates(&updatedUser).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al actualizar el usuario"})
+		return
+	}
+
+	c.JSON(http.StatusOK, existingUser)
+}
+
+func DeleteUser(c *gin.Context, db *gorm.DB) {
+	var user models.User
+	userID, err := strconv.ParseUint(c.Param("id"), 10, 64) //De String a Int, en base 10 y tamaño de bits 64
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID de usuario inválido"})
+		return
+	}
+
+	if err := db.First(&user, userID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no encontrado"})
+		return
+	}
+	// Eliminar el usuario de la base de datos
+	if err := db.Delete(&user, userID).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al eliminar el usuario"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Usuario eliminado exitosamente"})
 }
--- main.go
+++ main.go
@@ -13,11 +13,12 @@ func main() {
 	r := gin.Default()
 	db := controllers.ConnectDB()
 
+	//GET
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
-	r.GET("/users/create", func(c *gin.Context) {
-		controllers.CreateUsers(c, db)
+	r.GET("/users/createtable", func(c *gin.Context) {
+		controllers.CreateUsersTable(c, db)
 	})
 	r.GET("/users/get", func(c *gin.Context) {
 		controllers.GetUsers(c, db)
@@ -25,11 +26,23 @@ func main() {
 	r.GET("/user/get", func(c *gin.Context) {
 		controllers.GetUserId(c, db)
 	})
+	//POST
+	r.POST("/users/create", func(c *gin.Context) {
+		controllers.CreateUsers(c, db)
+	})
 	r.POST("/users/post", func(c *gin.Context) {
 		controllers.PostUsers(c, db)
 	})
 	r.POST("/user/post", func(c *gin.Context) {
 		controllers.PostUserId(c, db)
+	})
+	//PATCH: actualización parcial de un registro. PUT: actualización completa.
+	r.PATCH("/user/update", func(c *gin.Context) {
+		controllers.UpdateUser(c, db)
+	})
+	//DELETE
+	r.DELETE("/user/delete/:id", func(c *gin.Context) {
+		controllers.DeleteUser(c, db)
 	})
 
 	r.Run()
