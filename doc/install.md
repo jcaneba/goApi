934ada589858296c4015cc5dbd9621c9fef40f8b
jcaneba <jesuscanebatic@gmail.com>
2024-03-29 18:04:46 +0100

    Instalación de Swagger. Falta añadir documentación.

--- doc/install.md
+++ doc/install.md
@@ -5,6 +5,10 @@ go mod init goApi
 go get github.com/gin-gonic/gin
 go get gorm.io/driver/mysql gorm.io/gorm
 go get github.com/joho/godotenv
+go install github.com/swaggo/swag/cmd/swag@latest
+~/go/bin/swag init
+go get github.com/swaggo/gin-swagger
+go get github.com/swaggo/files
 mkdir doc controllers models
 touch .env main.go
 echo -e "PORT=8081\nDB_USER='root'\nDB_PASS='1234'\nDB_HOST='172.19.0.2'\nDB_TABLE='test'" > .env
--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"goApi/controllers"
+	_ "goApi/docs"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+// @host      localhost:8080
+// @BasePath  /api/v1
+// @securityDefinitions.basic  BasicAuth
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
 	godotenv.Load()
 	r := gin.Default()
 	db := controllers.ConnectDB()
 
 	//GET
+	r.GET("docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
